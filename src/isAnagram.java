e185efdaaa15b444778e446af8580194cc1e072d
Farrukh Zarioov <farrukhzarioov@macbook-pro.mynetworksettings.com>
2022-12-19 11:15:12 -0500

    commiting is Anagram

---
+++ src/isAnagram.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class isAnagram {
+    /**
+     *
+     * @param str1 is the first parameter
+     * @param str2 is the second parameter
+     * @return return true is both Strings have same length and exact same characters.
+     */
+    public static boolean isAnagram(String str1, String str2){
+
+        // if the length are not the same we know strings are not Anagram and return false;
+        if (str1.length() != str2.length()){
+            return false;
+        }
+        char[] charArray1 = str1.toLowerCase().toCharArray(); //  converting str1 to lover cases and to char array
+        char[] charArray2 = str1.toLowerCase().toCharArray(); //  converting str2 to lower cases and to char array
+
+        Arrays.sort(charArray1); // sorting in alphabetical order
+        Arrays.sort(charArray2); // sorting in alphabetical order
+
+        return Arrays.equals(charArray1, charArray2);  // returning true is both sorted arrays are matching
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("Aziza", "Zizaa"));
+    }
+
+}
